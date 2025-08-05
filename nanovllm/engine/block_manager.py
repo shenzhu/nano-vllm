12b6a819518cb231a2dfe3e2cb9d3ad96ecfe066
Shen Zhu <shenzhu0127@gmail.com>
2025-08-04 21:21:46 -0700

    Update block_manager.py

--- nanovllm/engine/block_manager.py
+++ nanovllm/engine/block_manager.py
@@ -10,6 +10,7 @@ class Block:
     def __init__(self, block_id):
         self.block_id = block_id
         self.ref_count = 0
+        # Identify if blocks have the same content
         self.hash = -1
         self.token_ids = []
 
@@ -35,16 +36,20 @@ class BlockManager:
 
     @classmethod
     def compute_hash(cls, token_ids: list[int], prefix: int = -1):
+        """
+        Compute the hash of a block, only after a block is fully filled with tokens.
+        """
         h = xxhash.xxh64()
         if prefix != -1:
             h.update(prefix.to_bytes(8, "little"))
+        # Append the data to previous value, numpy will use little-endian by default
         h.update(np.array(token_ids).tobytes())
         return h.intdigest()
 
     def _allocate_block(self, block_id: int) -> Block:
         block = self.blocks[block_id]
         assert block.ref_count == 0
-        block.reset()
+        block.reset() # Reference count is set to 1
         self.free_block_ids.remove(block_id)
         self.used_block_ids.add(block_id)
         return self.blocks[block_id]
@@ -58,16 +63,28 @@ class BlockManager:
         return len(self.free_block_ids) >= seq.num_blocks
 
     def allocate(self, seq: Sequence):
+        """
+        Allocate blocks for a sequence.
+        """
+        # Make sure the it's the first time to allocate blocks
         assert not seq.block_table
         h = -1
         cache_miss = False
+
+        # seq.num_blocks is the number of blocks needed to store the sequence,
+        # this can be calcualted statically
         for i in range(seq.num_blocks):
             token_ids = seq.block(i)
+            # Only compute the hash if the block is full
             h = self.compute_hash(token_ids, h) if len(token_ids) == self.block_size else -1
+
             block_id = self.hash_to_block_id.get(h, -1)
             if block_id == -1 or self.blocks[block_id].token_ids != token_ids:
+                # Cache miss, or the block is not the same as existing one
                 cache_miss = True
+
             if cache_miss:
+                # Allocate new block if cache miss
                 block_id = self.free_block_ids[0]
                 block = self._allocate_block(block_id)
             else:
@@ -76,38 +93,51 @@ class BlockManager:
                     block = self.blocks[block_id]
                     block.ref_count += 1
                 else:
+                    # Maybe hash table has the block_id but used_block_ids is cleared
                     block = self._allocate_block(block_id)
+
             if h != -1:
+                # Update the hash value of block
                 block.update(h, token_ids)
                 self.hash_to_block_id[h] = block_id
             seq.block_table.append(block_id)
 
     def deallocate(self, seq: Sequence):
+        # Deallocate from end to start
         for block_id in reversed(seq.block_table):
             block = self.blocks[block_id]
             block.ref_count -= 1
             if block.ref_count == 0:
+                # Only release the block if ref count is 0
                 self._deallocate_block(block_id)
         seq.num_cached_tokens = 0
         seq.block_table.clear()
 
     def can_append(self, seq: Sequence) -> bool:
+        # len(self.free_block_ids): How many free blocks does the manager have
+        # Only need one more block if the last block has only one token
         return len(self.free_block_ids) >= (len(seq) % self.block_size == 1)
 
     def may_append(self, seq: Sequence):
         block_table = seq.block_table
+        # Get the last block in the block table
         last_block = self.blocks[block_table[-1]]
         if len(seq) % self.block_size == 1:
+            # Has only one token in the last block
             assert last_block.hash != -1
             block_id = self.free_block_ids[0]
             self._allocate_block(block_id)
             block_table.append(block_id)
         elif len(seq) % self.block_size == 0:
+            # The last block is full, we need to update the hash and token_ids
             assert last_block.hash == -1
             token_ids = seq.block(seq.num_blocks-1)
+
+            # Calculate the hash of the last block (based on the previous block's hash)
             prefix = self.blocks[block_table[-2]].hash if len(block_table) > 1 else -1
             h = self.compute_hash(token_ids, prefix)
             last_block.update(h, token_ids)
+
             self.hash_to_block_id[h] = last_block.block_id
         else:
             assert last_block.hash == -1
